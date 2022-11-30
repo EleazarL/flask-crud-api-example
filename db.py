df2ec674c75e9a8f55e5776ad1b3584226619119
EleazarL <66807291+EleazarL@users.noreply.github.com>
2022-11-30 04:55:06 +0000

    cambio de mysql

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import pymysql
 
 def obtener_conexion():
-    return pymysql.connect(host='containers-us-west-114.railway.app',
-                                user='root',
-                                password='LtQm0b6CfxCZaGzx6aVd',
-                                db='railway')
+    return pymysql.connect(host='bbh4lfs9qgiqk5rmdcoq-mysql.services.clever-cloud.com',
+                                user='u4paijab6cwvtlmp',
+                                password='Ynmot5L3VsJsW3aJnHcL',
+                                db='bbh4lfs9qgiqk5rmdcoq')
