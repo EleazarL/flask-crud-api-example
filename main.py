dcdd8f3776f186b8dcf87e601da7479d5077505a
EleazarL <66807291+EleazarL@users.noreply.github.com>
2022-11-30 04:19:58 +0000

    asd

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask,render_template, request
+from flask import Flask
 from flask_mysqldb import MySQL
- 
+
 app = Flask(__name__)
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
- 
+
+# Required
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "LtQm0b6CfxCZaGzx6aVd"
+app.config["MYSQL_DB"] = "railway"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/path/to/ca-file"}}
+
 mysql = MySQL(app)
+
+@app.route("/")
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute("""SELECT * FROM login""")
+    rv = cur.fetchall()
+    return str(rv)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
